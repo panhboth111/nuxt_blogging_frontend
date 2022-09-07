9ffd9e7bbef1925d42642a8337d96217336af7d4
Neak Panhboth <neakpanhboth18@kit.edu.kh>
2022-09-07 19:12:15 +0700

    initial commit

---
+++ layouts/footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="relative bg-gray-100 dark:bg-black transition ease-in-out duration-700"
+  >
+    <div>
+      <Navbar />
+    </div>
+    <div class="pb-20">
+      <Nuxt />
+    </div>
+    <div class="mt-10">
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Layouts/Navbar.vue";
+import Footer from "~/components/Layouts/Footer.vue";
+export default {
+  components: { Navbar, Footer },
+};
+</script>
+
+<style></style>
